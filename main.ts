3af6ce7abb41bf802a75131ab24af83465f219b3
Aidan Lalonde-Novales <71395339+Aidan-Lalonde-Novales@users.noreply.github.com>
2020-10-07 14:23:38 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let servo_rotation = 0
+let resistance = 0
+basic.showIcon(IconNames.Happy)
+// Created by Aidan L-N
+// Created October 7th 2020
+// 
+// Code that reads the value given by the potentiometer and converts it to a degree
+// value that the servo can use to rotate itself.
+basic.forever(function () {
+    resistance = pins.analogReadPin(AnalogPin.P1)
+    servo_rotation = resistance / 4
+    robotbit.Servo(robotbit.Servos.S1, servo_rotation)
+    basic.showNumber(resistance)
+    basic.pause(500)
+})
